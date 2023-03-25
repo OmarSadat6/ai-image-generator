f448a4f04db4937bed3946131564cb414b1d426c
OmarSadat6 <omarsadat54@gmail.com>
2023-03-25 15:33:55 -0400

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import Header from '@/components/Header'
+import '../styles/globals.css'
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +13,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* header */}
+        <Header/>
+        {/* prompt */}
+        {children}
+      </body>
     </html>
   )
 }
