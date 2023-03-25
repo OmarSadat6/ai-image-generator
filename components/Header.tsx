042eae4804232791fce5621bead83e3a40f14bd9
OmarSadat6 <omarsadat54@gmail.com>
2023-03-25 15:50:01 -0400

    header started

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import Image from "next/image";
 
 function Header() {
   return <header>
+    <div>
+        <Image
+            src="https://links.papareact.com/4t3"
+            alt="logo"
+            height={30}
+            width={30}
+            />
+       <div>
+        <h1>
+            <span className='text-violet-500'> AI</span> Image Generator
+        </h1>
+        <h2>
+
+        </h2>
+       </div>
+    </div>
 
   </header>;
 }
